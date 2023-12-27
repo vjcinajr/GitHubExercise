3157c9de6eb9383ddd9b19a16230717c2609b1c0
Vincent Cina <vcina@me.com>
2023-12-27 13:28:25 -0500

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by VINCENT CINA on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
