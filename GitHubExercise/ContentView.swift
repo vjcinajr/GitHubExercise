e06852065f191526b210dfc98da2e2f2ea578833
Vincent Cina <vcina@me.com>
2023-12-27 13:51:34 -0500

    V1.0 - red

--- GitHubExercise/ContentView.swift
+++ GitHubExercise/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+            Text("V1.0")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
+        .foregroundColor(.red)
     }
 }
 
